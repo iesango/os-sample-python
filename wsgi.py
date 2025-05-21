f3dc8e168fa267e4a6faa846c79a88ecf4ee16b4
iesango <iesango@indra.es>
2025-05-21 16:59:43 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Openshift Ivan!"
 
 if __name__ == "__main__":
     application.run()
