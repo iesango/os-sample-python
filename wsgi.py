61adc74a88b732733a35a41392576cad4c287924
iesango <iesango@indra.es>
2025-05-22 12:30:46 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
 from flask import Flask
+
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello Openshift Ivan!"
+def home():
+    return """
+    <h1>¡Hola desde OpenShift Iván!</h1>
+    <p>Esta es mi primera aplicación web desplegada en la nube.</p>
+    <a href='/about'>Ir a Acerca de</a>
+    """
+
+@application.route("/about")
+def about():
+    return """
+    <h1>Acerca de esta app</h1>
+    <p>Esta aplicación fue creada usando Python y Flask, y desplegada en OpenShift.</p>
+    <p>Autor: Iván</p>
+    <a href='/'>Volver al inicio</a>
+    """
 
 if __name__ == "__main__":
     application.run()
